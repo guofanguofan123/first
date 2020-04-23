8e78a304e92a80305fa89ab7587c80e9cda7249e
潘超越 <pancyue@outlook.com>
2020-04-23 17:18:08 +0800

    删除一列

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,17 +22,21 @@ selector: 'app-root',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  itemText = '';
   public todo: {
     id: number,
     title: string,
     down: boolean
   }[] = todo;
+  public editContent: {
+    id: number,
+    title: string,
+    down: boolean
+  }[] = null;
 
   addtodo(e): void{
     const todoContent = e.target.value;
-    if (!todoContent.length){
-      return;
-    }
+    if ( todoContent.length > 0){
     const last = this.todo[this.todo.length - 1];
     this.todo.push(
       {
@@ -42,11 +46,26 @@ export class AppComponent {
       }
      );
     e.target.value = ' ';
+    }
   }
   get toggleAll(){
    return this.todo.every(item => item.down);
   }
   set toggleAll(val){
-    this.todo.forEach(item => item.down = val)
+    this.todo.forEach(item => item.down = val);
+  }
+  delete(key: number){
+    this.todo.splice(key, 1);
+  }
+  saveEdit(item, e){
+    item.title = e.target.value;
+    this.editContent = null;
+
+  }
+  cancleEditor(e){
+    const {keyCode, target} = e;
+    if (keyCode === 27){
+      this.editContent = null;
+    }
   }
 }
